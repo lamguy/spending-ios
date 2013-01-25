95d2c3c6f34bee5cc680b96eda9205e5476276d7
Lam Nguyen <me@madebylam.com>
2013-01-25 07:27:09 -0600

    reverse array of spend records on main view

--- Spending/Spending_ViewController.m
+++ Spending/Spending_ViewController.m
@@ -12,8 +12,40 @@
 @interface Spending_ViewController ()
 {
     NSMutableArray *arrayOfRecord;
+    NSMutableArray *reversed_arrayOfRecord;
     sqlite3 *recordDB;
     NSString *dbPathString;
+}
+
+@end
+
+@implementation NSArray (Reverse)
+
+- (NSArray *)reversedArray {
+    NSMutableArray *array = [NSMutableArray arrayWithCapacity:[self count]];
+    NSEnumerator *enumerator = [self reverseObjectEnumerator];
+    for (id element in enumerator) {
+        [array addObject:element];
+    }
+    return array;
+}
+
+@end
+
+@implementation NSMutableArray (Reverse)
+
+- (void)reverse {
+    if ([self count] == 0)
+        return;
+    NSUInteger i = 0;
+    NSUInteger j = [self count] - 1;
+    while (i < j) {
+        [self exchangeObjectAtIndex:i
+                  withObjectAtIndex:j];
+        
+        i++;
+        j--;
+    }
 }
 
 @end
@@ -80,6 +112,9 @@
                 
                 NSLog(@"pulled");
             }
+
+            //Now we need to reverse the array of our records for better UX
+            [arrayOfRecord reverse];
         }
     }
 
