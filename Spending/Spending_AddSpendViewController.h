5e78f15b47a04b0108c7f608d058fc49f99af1f8
Lam Nguyen <me@madebylam.com>
2013-01-24 22:05:49 -0600

    removed some useless libraries

---
+++ Spending/Spending_AddSpendViewController.h
@@ -0,0 +1,13 @@
+//
+//  Spending_AddSpendViewController.h
+//  Spending
+//
+//  Created by Lam Nguyen on 1/21/13.
+//  Copyright (c) 2013 Geniuspilot Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Spending_AddSpendViewController : UIViewController
+
+@end
---
+++ Spending/Spending_AddSpendViewController.m
@@ -0,0 +1,38 @@
+//
+//  Spending_AddSpendViewController.m
+//  Spending
+//
+//  Created by Lam Nguyen on 1/21/13.
+//  Copyright (c) 2013 Geniuspilot Interactive. All rights reserved.
+//
+
+#import "Spending_AddSpendViewController.h"
+
+@interface Spending_AddSpendViewController ()
+
+@end
+
+@implementation Spending_AddSpendViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
