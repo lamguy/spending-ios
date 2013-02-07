9f6849500960d88729775f691a9e0b842e8ee110
Lam Nguyen <me@madebylam.com>
2013-02-07 07:47:56 -0600

    Initial Commit

---
+++ Spending/GI_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GI_AppDelegate.h
+//  Spending
+//
+//  Created by Lam Nguyen on 2/7/13.
+//  Copyright (c) 2013 Geniuspilot Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GI_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
